001aa0c2594f16280f1cc98d2bd612933ccbd840
mapaperezm <maperezme@unal.edu.co>
2026-07-28 23:00:34 -0500

    punto 3
    función donde se crean los 3 servidores

--- src/cripto/punto_3.py
+++ src/cripto/punto_3.py
@@ -1,5 +1,6 @@
 import random
 MODULO = 1000003
+#construcción de servidores individuales
 def dividir_secreto(numero):
     parte1 = random.randint(0, MODULO - 1)
     parte2 = random.randint(0, MODULO - 1)
@@ -8,9 +9,17 @@ def dividir_secreto(numero):
 
     return parte1, parte2, parte3
 p1, p2, p3 = dividir_secreto(40)
+# construir 3 servidores 
+def crear_servidores(notas):
+    servidor1 = []
+    servidor2 = []
+    servidor3 = []
 
-print(p1)
-print(p2)
-print(p3)
+    for nota in notas:
+        p1, p2, p3 = dividir_secreto(nota)
 
-print((p1+p2+p3)%MODULO)
+        servidor1.append(p1)
+        servidor2.append(p2)
+        servidor3.append(p3)
+
+    return servidor1, servidor2, servidor3
